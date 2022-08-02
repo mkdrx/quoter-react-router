ccf10302d7f5316ce8eaa1c01b6e20b170b168f4
mkdrx <markusdrx@gmail.com>
2022-08-02 19:54:40 +0200

    added Prompt component, working with query params useHistory useLocation

--- src/components/quotes/QuoteForm.js
+++ src/components/quotes/QuoteForm.js
@@ -1,13 +1,21 @@
-import { useRef } from "react";
+import { Fragment, useRef, useState } from "react";
+// Prompt Component: watches if we navigate away, if a set condition is met it will show a warning
+// needs 2 props: when(true/false) & message with a function(location) returning text
+import { Prompt } from "react-router-dom";
 
 import Card from "../UI/Card";
 import LoadingSpinner from "../UI/LoadingSpinner";
 import classes from "./QuoteForm.module.css";
 
 const QuoteForm = (props) => {
+  // State for onFocus
+  const [isEntering, setIsEntering] = useState(false);
+
+  // Refs
   const authorInputRef = useRef();
   const textInputRef = useRef();
 
+  // Handlers
   function submitFormHandler(event) {
     event.preventDefault();
 
@@ -19,28 +27,49 @@ const QuoteForm = (props) => {
     props.onAddQuote({ author: enteredAuthor, text: enteredText });
   }
 
-  return (
-    <Card>
-      <form className={classes.form} onSubmit={submitFormHandler}>
-        {props.isLoading && (
-          <div className={classes.loading}>
-            <LoadingSpinner />
-          </div>
-        )}
+  const formFocusedHandler = () => {
+    setIsEntering(true);
+  };
 
-        <div className={classes.control}>
-          <label htmlFor="author">Author</label>
-          <input type="text" id="author" ref={authorInputRef} />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor="text">Text</label>
-          <textarea id="text" rows="5" ref={textInputRef}></textarea>
-        </div>
-        <div className={classes.actions}>
-          <button className="btn">Add Quote</button>
-        </div>
-      </form>
-    </Card>
+  const finishEnteringHandler = () => {
+    setIsEntering(false);
+  };
+
+  return (
+    <Fragment>
+      <Prompt
+        when={isEntering}
+        message={(location) =>
+          "Are you sure you want to leave? All your entered data will be lost!"
+        }
+      />
+      <Card>
+        <form
+          onFocus={formFocusedHandler}
+          className={classes.form}
+          onSubmit={submitFormHandler}
+        >
+          {props.isLoading && (
+            <div className={classes.loading}>
+              <LoadingSpinner />
+            </div>
+          )}
+          <div className={classes.control}>
+            <label htmlFor="author">Author</label>
+            <input type="text" id="author" ref={authorInputRef} />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="text">Text</label>
+            <textarea id="text" rows="5" ref={textInputRef}></textarea>
+          </div>
+          <div className={classes.actions}>
+            <button onClick={finishEnteringHandler} className="btn">
+              Add Quote
+            </button>
+          </div>
+        </form>
+      </Card>
+    </Fragment>
   );
 };
 
--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,13 +1,48 @@
-import { Fragment } from 'react';
+import { Fragment } from "react";
+// useLocation gives information about the currently loaded url
+import { useHistory, useLocation } from "react-router-dom";
 
-import QuoteItem from './QuoteItem';
-import classes from './QuoteList.module.css';
+import QuoteItem from "./QuoteItem";
+import classes from "./QuoteList.module.css";
+
+// Helper sort by id
+const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((quoteA, quoteB) => {
+    if (ascending) {
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  });
+};
 
 const QuoteList = (props) => {
+  const history = useHistory();
+  const location = useLocation();
+
+  // To extract data from query parameters
+  const queryParams = new URLSearchParams(location.search);
+
+  // Helper to get key/value - check if the sorting is ascending or not
+  const isSortingAsc = queryParams.get("sort") === "asc";
+
+  // Helper to output the items
+  const sortedQuotes = sortQuotes(props.quotes, isSortingAsc);
+
+  // Handler for updating the query parameters in the URL ascending or descending (through useHistory hook)
+  const changeSortingHandler = () => {
+    history.push("/quotes?sort=" + (isSortingAsc ? "desc" : "asc"));
+  };
+
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={changeSortingHandler}>
+          Sort {isSortingAsc ? "Descending" : "Ascending"}
+        </button>
+      </div>
       <ul className={classes.list}>
-        {props.quotes.map((quote) => (
+        {sortedQuotes.map((quote) => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
